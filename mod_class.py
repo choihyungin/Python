2cb07e52e7b17dddd9c491d14114059b6c75d8c2
Hyungin Choi <01_01@naver.com>
2020-01-14 16:24:17 +0900

    Modify 05

---
+++ mod_class.py
@@ -0,0 +1,10 @@
+class MyClass:
+    
+    def __init__(self, text):
+        self.text = text
+    
+    def show(self):
+        print(self.text)
+    
+    def get_name(self):
+        return self.text 
---
+++ mymod.py
@@ -0,0 +1,5 @@
+def myfunc(a, b):
+    return a+b
+
+if __name__ == "__main__":
+    print(myfunc(2, 3))
